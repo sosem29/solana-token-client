0980fcacc89de4a16174366ea6966c870e7f93de
sosem29 <sumesabri@gmail.com>
2022-12-21 00:59:13 +0300

    initialize commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
 import * as token from "@solana/spl-token"
+import {
+    Metaplex,
+    keypairIdentity,
+    bundlrStorage,
+    toMetaplexFile,
+    findMetadataPda,
+  } from "@metaplex-foundation/js"
+  import {
+    DataV2,
+    createCreateMetadataAccountV2Instruction,
+    createUpdateMetadataAccountV2Instruction,
+  } from "@metaplex-foundation/mpl-token-metadata"
+  import * as fs from "fs"
 
 async function createNewMint(
     connection: web3.Connection,
@@ -110,9 +123,105 @@ async function burnTokens(
     )
 }
 
+async function createTokenMetadata(
+    connection: web3.Connection,
+    metaplex: Metaplex,
+    mint: web3.PublicKey,
+    user: web3.Keypair,
+    name: string,
+    symbol: string,
+    description: string
+  ) {
+    // file to buffer
+    const buffer = fs.readFileSync("assets/soscoin.png")
+  
+    // buffer to metaplex file
+    const file = toMetaplexFile(buffer, "soscoin.png")
+  
+    // upload image and get image uri
+    const imageUri = await metaplex.storage().upload(file)
+    console.log("image uri:", imageUri)
+  
+    // upload metadata and get metadata uri (off chain metadata)
+    const { uri } = await metaplex
+      .nfts()
+      .uploadMetadata({
+        name: name,
+        description: description,
+        image: imageUri,
+      })
+   
+    console.log("metadata uri:", uri)
+  
+    // get metadata account address
+    const metadataPDA = await findMetadataPda(mint)
+  
+    // onchain metadata format
+    const tokenMetadata = {
+      name: name,
+      symbol: symbol,
+      uri: uri,
+      sellerFeeBasisPoints: 0,
+      creators: null,
+      collection: null,
+      uses: null,
+    } as DataV2
+  
+    // transaction to create metadata account
+    const transaction = new web3.Transaction().add(
+      createCreateMetadataAccountV2Instruction(
+        {
+          metadata: metadataPDA,
+          mint: mint,
+          mintAuthority: user.publicKey,
+          payer: user.publicKey,
+          updateAuthority: user.publicKey,
+        },
+        {
+          createMetadataAccountArgsV2: {
+            data: tokenMetadata,
+            isMutable: true,
+          },
+        }
+      )
+    )
+  
+    // send transaction
+    const transactionSignature = await web3.sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [user]
+    )
+  
+    console.log(
+      `Create Metadata Account: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    )
+  }
+
 async function main() {
     const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
     const user = await initializeKeypair(connection)
+
+    console.log("PublicKey:", user.publicKey.toBase58())
+    
+    const MINT_ADDRESS = "Ft8Zm5pgsd4RYnLZdHGQZ4EjbCb9DqPeJR4bcW888HaR"
+
+    const metaplex = Metaplex.make(connection).use(keypairIdentity(user)).use(bundlrStorage({
+            address: "https://devnet.bundlr.network",
+            providerUrl: "https://api.devnet.solana.com",
+            timeout: 60000,
+          })
+        )
+        // Calling the token 
+        await createTokenMetadata(
+          connection,
+          metaplex,
+          new web3.PublicKey(MINT_ADDRESS),
+          user,
+          "Soscoin", // Token name - REPLACE THIS WITH YOURS
+          "SSC",     // Token symbol - REPLACE THIS WITH YOURS
+          "Lets go to the Moon" // Token description - REPLACE THIS WITH YOURS
+        )
 
     const mint = await createNewMint(
         connection,
--- src/initializeKeypair.ts
+++ src/initializeKeypair.ts
@@ -10,7 +10,6 @@ export async function initializeKeypair(
         console.log("Creating .env file")
         const signer = web3.Keypair.generate()
         fs.writeFileSync(".env", `PRIVATE_KEY=[${signer.secretKey.toString()}]`)
-        await airdropSolIfNeeded(signer, connection)
 
         return signer
     }
@@ -18,18 +17,5 @@ export async function initializeKeypair(
     const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[]
     const secretKey = Uint8Array.from(secret)
     const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey)
-    await airdropSolIfNeeded(keypairFromSecretKey, connection)
     return keypairFromSecretKey
-}
-
-async function airdropSolIfNeeded(
-    signer: web3.Keypair,
-    connection: web3.Connection
-) {
-    const balance = await connection.getBalance(signer.publicKey)
-    console.log("Current balance is", balance)
-    if (balance < web3.LAMPORTS_PER_SOL) {
-        console.log("Airdropping 1 SOL...")
-        await connection.requestAirdrop(signer.publicKey, web3.LAMPORTS_PER_SOL)
-    }
 }
